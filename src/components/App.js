b15b77f1fe7a8afeed4dd5648591e05548bab0d7
DABax47 <45886982+DABax47@users.noreply.github.com>
2020-11-24 10:48:12 -0500

    u

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import Form from "./Form";
 import Roster from "./Roster";
 import Match from "./Match";
@@ -9,7 +11,6 @@ import split from "../helpers/splitplayers.js";
 //use HashRouter instead much simpler
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   Link
 } from "react-router-dom";
@@ -21,10 +22,10 @@ class App extends Component {
 
 
   state = {
-    players: [ ],
+    players: [],
 
     currentMatches: {},
-    roster:[]
+    roster: []
   };
   //Methods
   /**************
@@ -43,86 +44,129 @@ class App extends Component {
             team: teamName,
             winToken: false,
             played: false,
-            id: prevState.players.length +1
+            id: prevState.players.length + 1
           }
         ],
-        roster:[  ...prevState.roster,
+        roster: [...prevState.roster,
           {
             playerNames: [p1, p2],
             team: teamName,
             winToken: false,
             played: false,
-            id:prevState.players.length +1
-          }]
+            id: prevState.players.length + 1
+          }
+        ]
       };
     });
 
   };
 
-  remove=(id)=>{
+  remove = (id) => {
     console.log(id);
-    this.setState(prevState=>{
-      return{players:prevState.players.filter((p)=>{
-        return p.id !== id
-      })}
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter((p) => {
+          return p.id !== id
+        })
+      }
     })
   }
-win=(id,len)=>{
-  if (this.state.players.length === 2) {
-    let winner =this.state.players.find(elm=> elm.id !== id).team;
-    alert(winner +' is the winner');
+  win = (id, len) => {
+    if (this.state.players.length === 2) {
+      let winner = this.state.players.find(elm => elm.id !== id).team;
+      alert(winner + ' is the winner');
 
+    }
   }
-}
 
   match = () => {
 
     let [L1, L2] = split(this.state.players);
 
-    this.addMatch(L1,L2);
+    this.addMatch(L1, L2);
   };
-  addMatch = (L1,L2) => {
+  addMatch = (L1, L2) => {
     this.setState(prevState => {
       return {
-        currentMatches: {L1,L2}
+        currentMatches: {
+          L1,
+          L2
+        }
       };
     });
   };
   // Render Mehtod
   render() {
-    return (
-    <Router>  <div id='app'>
-      <Route path='/' exact>
-       <section id="reg">
-          <Form addPlayer={this.addPlayer} num={this.state.players.length}/>
-          <Link  to ='/roster'><button onClick={this.localState}className='start'>START<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button></Link>
+    return ( <
+      Router > < div id = 'app' >
+      <
+      Route path = '/'
+      exact >
+      <
+      section id = "reg" >
+      <
+      Form addPlayer = {
+        this.addPlayer
+      }
+      num = {
+        this.state.players.length
+      }
+      /> <
+      Link to = '/roster' > < button onClick = {
+        this.localState
+      }
+      className = 'start' > START < i class = "fa fa-arrow-circle-o-right"
+      aria - hidden = "true" > < /i></button > < /Link>
 
-        </section>
-        </Route>
-    <Route path='/roster' exact>
-    <section id="ros">
-  <span className='ros-links'>
-  <Link  to ='/'><button >Add Players</button></Link>
-  <Link  to ='/match'><button >Start</button></Link>
-  </span>
-          <Roster  players={this.state.roster}/>
+      <
+      /section> <
+      /Route> <
+      Route path = '/roster'
+      exact >
+      <
+      section id = "ros" >
+      <
+      span className = 'ros-links' >
+      <
+      Link to = '/' > < button > Add Players < /button></Link >
+      <
+      Link to = '/match' > < button > Start < /button></Link >
+      <
+      /span> <
+      Roster players = {
+        this.state.roster
+      }
+      />
 
-        </section>
-        </Route>
-      <Route path='/match'>
-       <section id="match">
-          <Match
-            remove={this.remove}
-            players={this.state.players}
-            matches ={this.state.currentMatches}
-            match ={this.match}
-            win = {this.win}
-          />
-            <Link  to ='/'><button >Restart</button></Link>
-        </section>
-        </Route>
-      </div>
-      </Router>
+      <
+      /section> <
+      /Route> <
+      Route path = '/match' >
+      <
+      section id = "match" >
+      <
+      Match remove = {
+        this.remove
+      }
+      players = {
+        this.state.players
+      }
+      matches = {
+        this.state.currentMatches
+      }
+      match = {
+        this.match
+      }
+      win = {
+        this.win
+      }
+      /> <
+      Link to = '/' > < button > Restart < /button></Link >
+      <
+      /section> <
+      /Route> <
+      /div> <
+      /Router>
     );
   }
 }
